f5ca985b7b53e0a9052790250c5d0b2d678d5dc6
Tcampbell93 <tcampbell_93@hotmail.com>
2024-02-05 19:08:17 -0800

    finished weather function

--- assets/script.js
+++ assets/script.js
@@ -79,44 +79,49 @@ function getWeather(latitude, longitude) {
 }
 
 function printResults(resultObj) {
-    var weatherCard = document.createElement('div');
-    weatherCard.classList.add('card');
-
-    var weatherBody = document.createElement('div');
-    weatherBody.classList.add('card-body');
-
-    var cardDate = document.createElement('h4');
-    cardDate.classList.add('card-date');
-    var date = resultObj.dt_txt;
-    cardDate.textContent = date;
-
-    var weatherIcon = resultObj.weather[0].icon;
-    var iconUrl = getWeatherIcon(weatherIcon);
-
-    var iconImg = document.createElement('img');
-    iconImg.src = iconUrl;
-
-    var temp = document.createElement('div');
-    temp.textContent = "Temperature: " + resultObj.main.temp + "\u00B0 F";   
-
-    var clouds = document.createElement('div');
-    clouds.textContent = "Clouds: " + resultObj.clouds.all + "% cloud coverage.";
-
-    var windSpeed = document.createElement('div');
-    windSpeed.textContent = 'Wind: ' + resultObj.wind.speed + ' mph';  
-
-    weatherBody.append(cardDate);
-    weatherBody.append(iconImg);
-    weatherBody.append(temp);
-    weatherBody.append(clouds);
-    weatherBody.append(windSpeed);
-    weatherCard.append(weatherBody);
-    weatherEl.append(weatherCard);
+    var cardDate = resultObj.dt_txt.split(' ')[0];
+    
     days.forEach(function(day, index) {
-        var weatherContainer = day.querySelector('.weather-box');
-
-    })
-}
+      var weatherContainer = day.querySelector('.weather-box');
+      var currentDate = startOfWeek.add(index, 'day').format('YYYY-MM-DD');
+      
+      if (cardDate === currentDate) {
+        var weatherCard = document.createElement('div');
+        weatherCard.classList.add('card');
+        
+        var weatherBody = document.createElement('div');
+        weatherBody.classList.add('card-body');
+        
+        var cardDateElement = document.createElement('h4');
+        cardDateElement.classList.add('card-date');
+        cardDateElement.textContent = resultObj.dt_txt;
+        
+        var weatherIcon = resultObj.weather[0].icon;
+        var iconUrl = getWeatherIcon(weatherIcon);
+        
+        var iconImg = document.createElement('img');
+        iconImg.src = iconUrl;
+        
+        var temp = document.createElement('div');
+        temp.textContent = "Temperature: " + resultObj.main.temp + "\u00B0 F";
+        
+        var clouds = document.createElement('div');
+        clouds.textContent = "Clouds: " + resultObj.clouds.all + "% cloud coverage.";
+        
+        var windSpeed = document.createElement('div');
+        windSpeed.textContent = 'Wind: ' + resultObj.wind.speed + ' mph';
+        
+        weatherBody.append(cardDateElement);
+        weatherBody.append(iconImg);
+        weatherBody.append(temp);
+        weatherBody.append(clouds);
+        weatherBody.append(windSpeed);
+        
+        weatherCard.append(weatherBody);
+        weatherContainer.appendChild(weatherCard);
+      }
+    });
+  }
 
 function getWeatherIcon (weatherIcon) {
     var icon = 'https://openweathermap.org/img/wn/' + weatherIcon + "@2x.png";
